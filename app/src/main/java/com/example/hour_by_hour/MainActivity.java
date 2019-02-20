0c060dff666af92b802ef4d595007513920b04f5
Justin Jenkins <jen17046@byui.edu>
2019-02-19 18:23:37 -0700

    practice merge conflict

--- app/src/main/java/com/example/hour_by_hour/MainActivity.java
+++ app/src/main/java/com/example/hour_by_hour/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.hour_by_hour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 // Justin has edited this line again
+
+    public void displayInfo() {
+        Log.i("Main", "This is a new function");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
