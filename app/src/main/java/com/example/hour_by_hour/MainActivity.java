153319cf416451369ccb2c8cccc8b899c8c97d0f
Justin Jenkins <jen17046@byui.edu>
2019-02-19 18:03:51 -0700

    Justin conflict example

--- app/src/main/java/com/example/hour_by_hour/MainActivity.java
+++ app/src/main/java/com/example/hour_by_hour/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// Justin has edited this line
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
