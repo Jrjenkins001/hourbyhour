b4b2c4989b6356ad7ab41bfbdb8eec2cc6292918
Jrjenkins001 <47128430+Jrjenkins001@users.noreply.github.com>
2019-03-04 14:50:36 -0700

    Working on EditTask, adding the new toolbar

--- app/src/main/java/com/example/hour_by_hour/EditTask.java
+++ app/src/main/java/com/example/hour_by_hour/EditTask.java
@@ -2,6 +2,7 @@ package com.example.hour_by_hour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class EditTask extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class EditTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
