196a4db2a2e0330ba6dce3ca9edb4778bd3c2d78
Jrjenkins001 <47128430+Jrjenkins001@users.noreply.github.com>
2019-03-04 13:46:49 -0700

    Beginning to work with Edit Event Activity

--- app/src/main/java/com/example/hour_by_hour/EditTask.java
+++ app/src/main/java/com/example/hour_by_hour/EditTask.java
@@ -2,16 +2,12 @@ package com.example.hour_by_hour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class EditTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        //used to test other events
         setContentView(R.layout.activity_edit_task);
     }
 }
