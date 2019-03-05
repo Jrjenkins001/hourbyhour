2935b81ce7f9edb609f62e5acba12918b8b23184
Jrjenkins001 <47128430+Jrjenkins001@users.noreply.github.com>
2019-03-04 17:16:43 -0700

    Finish the UI on Edit Task

--- app/src/main/java/com/example/hour_by_hour/EditTask.java
+++ app/src/main/java/com/example/hour_by_hour/EditTask.java
@@ -1,8 +1,14 @@
 package com.example.hour_by_hour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class EditTask extends AppCompatActivity {
 
@@ -10,7 +16,54 @@ public class EditTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_edit_event, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_event:
+                // User chose the "Settings" item, show the app settings UI...
+                createEvent();
+                return true;
+
+            case R.id.action_delete_event:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                deleteEvent();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    void createEvent() {
+        CharSequence message = "Event Created";
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        toast.show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    void deleteEvent() {
+        CharSequence message = "Event Deleted";
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        toast.show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/hour_by_hour/MainActivity.java
+++ app/src/main/java/com/example/hour_by_hour/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.hour_by_hour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
-        //used to test other events
-        setContentView(R.layout.activity_edit_task);
+        setContentView(R.layout.activity_main);
     }
+
+    public void onClickTest(View view) {
+        Intent intent = new Intent(this, EditTask.class);
+        startActivity(intent);
+    }
+
 }
